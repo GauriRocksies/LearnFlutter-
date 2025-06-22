4aa44a057c988d2464056ffa52136f9f58331efc
GauriRocksies <gauri03uk@gmail.com>
2025-06-23 02:09:57 +0530

    Goodness there is so much in TextField!!!

--- git_flutter/lib/avatar/userInput.dart
+++ git_flutter/lib/avatar/userInput.dart
@@ -7,7 +7,55 @@ class userInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: TextField()
-    );
+        child: Container(
+            width: 300,
+            child: Column(
+              children: [
+                TextField(
+                  //enabled: false,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                        color: Colors.purpleAccent,
+                        width: 2,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide:
+                            BorderSide(color: Color.fromARGB(255, 24, 14, 29))),
+                    disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide:
+                            BorderSide(color: Color.fromARGB(255, 26, 23, 27))),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(
+                          color: Colors.red,
+                        )),
+                    suffixText: "username exists",
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        Icons.remove_red_eye,
+                        color: Colors.purple[400],
+                      ),
+                      onPressed: () {},
+                    ),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+                Container(height: 11),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(11),
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                    ),
+                  )),
+                )
+              ],
+            )));
   }
 }
