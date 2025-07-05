a6d8b7400d9608a836783bc8dbd2e8c03f37f84f
GauriRocksies <gauri03uk@gmail.com>
2025-07-06 02:35:08 +0530

    Having fun, yayy

--- git_flutter/lib/avatar/Wrap.dart
+++ git_flutter/lib/avatar/Wrap.dart
@@ -15,49 +15,58 @@ class WrapWidget extends StatelessWidget {
       Colors.blue,
       Colors.white,
     ];
-    return Wrap(
-      children: [
-        Container(
-          height: 100,
-          width: 190,
-          color: arrColor[0],
-        ),
-        Container(
-          height: 100,
-          width: 100,
-          color: arrColor[1],
-        ),
-        Container(
-          height: 100,
-          width: 100,
-          color: arrColor[2],
-        ),
-        Container(
-          height: 100,
-          width: 100,
-          color: arrColor[3],
-        ),
-        Container(
-          height: 100,
-          width: 100,
-          color: arrColor[4],
-        ),
-        Container(
-          height: 100,
-          width: 100,
-          color: arrColor[5],
-        ),
-        Container(
-          height: 100,
-          width: 100,
-          color: arrColor[6],
-        ),
-        Container(
-          height: 100,
-          width: 100,
-          color: arrColor[7],
-        ),
-      ],
+    return Container(
+      width: double.infinity,
+      child: Wrap(
+        direction: Axis.horizontal,
+        spacing: 11,
+        runSpacing: 11,
+        //alignment: WrapAlignment.center,
+        //alignment: WrapAlignment.spaceAround,
+        alignment: WrapAlignment.spaceEvenly,
+        children: [
+          Container(
+            height: 100,
+            width: 190,
+            color: arrColor[0],
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: arrColor[1],
+          ),
+          Container(
+            height: 150,
+            width: 180,
+            color: arrColor[2],
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: arrColor[3],
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: arrColor[4],
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: arrColor[5],
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: arrColor[6],
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: arrColor[7],
+          ),
+        ],
+      ),
     );
   }
 }
