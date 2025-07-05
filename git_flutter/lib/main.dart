edf413c4a929f85436f17e16fa0c93a58c0d659e
GauriRocksies <gauri03uk@gmail.com>
2025-07-06 02:29:23 +0530

    Modified main method for showing Wrap.dart

--- git_flutter/lib/main.dart
+++ git_flutter/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:git_flutter/avatar/Wrap.dart';
+import 'package:git_flutter/avatar/gridDemo.dart';
 //import 'package:git_flutter/avatar/StackWidget.dart';
 //import 'package:git_flutter/avatar/card.dart';
 //import 'package:git_flutter/pages/DivWidget.dart';
@@ -43,81 +45,83 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "HELLO",
-            style: Theme.of(context)
-                .textTheme
-                .headlineLarge!
-                .copyWith(color: Color.fromRGBO(228, 205, 233, 1)),
-          ),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
-        // body: SingleChildScrollView(
-        //     child: Column(
-        //   children: [
-        //     circleDemo(),
-        //     SizedBox(
-        //       height: 10,
-        //     ),
-        //     cardDemo(
-        //         emailText:
-        //             emailText), //when the cards are to be used, uncomment the section
-        //   ],
-        // )),
+      appBar: AppBar(
+        title: Text(
+          "HELLO",
+          style: Theme.of(context)
+              .textTheme
+              .headlineLarge!
+              .copyWith(color: Color.fromRGBO(228, 205, 233, 1)),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // body: SingleChildScrollView(
+      //     child: Column(
+      //   children: [
+      //     circleDemo(),
+      //     SizedBox(
+      //       height: 10,
+      //     ),
+      //     cardDemo(
+      //         emailText:
+      //             emailText), //when the cards are to be used, uncomment the section
+      //   ],
+      // )),
 
-        //body: Container(child: PageOne()),
+      //body: Container(child: PageOne()),
 
-        //body: Container(child: StackDemo()),
+      //body: Container(child: StackDemo()),
 
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 150,
-                height: 50,
-                child: RoundedButton(
-                  btnName: 'play',
-                  icon: Icon(
-                    Icons.play_circle_fill,
-                    size: 28,
-                  ),
-                  callBack: () {
-                    print("Logged In");
-                  },
-                  textStyle: TextStyle(
-                    fontSize: 30,
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              Container(
-                height: 15,
-              ),
-              Container(
-                width: 150,
-                height: 50,
-                child: RoundedButton(
-                  btnName: 'seek',
-                  icon: Icon(
-                    Icons.forward_10_rounded,
-                    size: 25,
-                    color: const Color.fromARGB(255, 19, 140, 240),
-                  ),
-                  callBack: () {
-                    print("Played");
-                  },
-                  bgColor: Colors.amber,
-                  textStyle: TextStyle(
-                    fontSize: 28,
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              )
-            ],
-          ),
-        ));
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Container(
+      //         width: 150,
+      //         height: 50,
+      //         child: RoundedButton(
+      //           btnName: 'play',
+      //           icon: Icon(
+      //             Icons.play_circle_fill,
+      //             size: 28,
+      //           ),
+      //           callBack: () {
+      //             print("Logged In");
+      //           },
+      //           textStyle: TextStyle(
+      //             fontSize: 30,
+      //             color: const Color.fromARGB(255, 0, 0, 0),
+      //             fontWeight: FontWeight.w600,
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 15,
+      //       ),
+      //       Container(
+      //         width: 150,
+      //         height: 50,
+      //         child: RoundedButton(
+      //           btnName: 'seek',
+      //           icon: Icon(
+      //             Icons.forward_10_rounded,
+      //             size: 25,
+      //             color: const Color.fromARGB(255, 19, 140, 240),
+      //           ),
+      //           callBack: () {
+      //             print("Played");
+      //           },
+      //           bgColor: Colors.amber,
+      //           textStyle: TextStyle(
+      //             fontSize: 28,
+      //             color: const Color.fromARGB(255, 0, 0, 0),
+      //             fontWeight: FontWeight.w600,
+      //           ),
+      //         ),
+      //       )
+      //     ],
+      //   ),
+
+      body: WrapWidget(),
+    );
   }
 }
