e3995db02100482c50c414a2ee91f3baca87d2dd
GauriRocksies <gauri03uk@gmail.com>
2025-07-06 02:28:34 +0530

    Learnt wrap widget using containers'

---
+++ git_flutter/lib/avatar/Wrap.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class WrapWidget extends StatelessWidget {
+  const WrapWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var arrColor = [
+      Colors.red,
+      Colors.amber,
+      Colors.purple,
+      Colors.pink,
+      Colors.brown,
+      Colors.green,
+      Colors.blue,
+      Colors.white,
+    ];
+    return Wrap(
+      children: [
+        Container(
+          height: 100,
+          width: 190,
+          color: arrColor[0],
+        ),
+        Container(
+          height: 100,
+          width: 100,
+          color: arrColor[1],
+        ),
+        Container(
+          height: 100,
+          width: 100,
+          color: arrColor[2],
+        ),
+        Container(
+          height: 100,
+          width: 100,
+          color: arrColor[3],
+        ),
+        Container(
+          height: 100,
+          width: 100,
+          color: arrColor[4],
+        ),
+        Container(
+          height: 100,
+          width: 100,
+          color: arrColor[5],
+        ),
+        Container(
+          height: 100,
+          width: 100,
+          color: arrColor[6],
+        ),
+        Container(
+          height: 100,
+          width: 100,
+          color: arrColor[7],
+        ),
+      ],
+    );
+  }
+}
