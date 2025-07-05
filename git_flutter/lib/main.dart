45bef19779f02b2c84b454787caca6c5cfae6465
GauriRocksies <gauri03uk@gmail.com>
2025-07-06 02:15:32 +0530

    Completed basics of cuustom widget

--- git_flutter/lib/main.dart
+++ git_flutter/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:git_flutter/avatar/StackWidget.dart';
+//import 'package:git_flutter/avatar/StackWidget.dart';
 //import 'package:git_flutter/avatar/card.dart';
-import 'package:git_flutter/pages/DivWidget.dart';
+//import 'package:git_flutter/pages/DivWidget.dart';
 import 'package:git_flutter/widgets_custom/RoundedBtn.dart';
 //import 'avatar/circleDemo.dart';
 import 'avatar/styleTh.dart';
@@ -43,42 +43,81 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "HELLO",
-          style: Theme.of(context)
-              .textTheme
-              .headlineLarge!
-              .copyWith(color: Color.fromRGBO(228, 205, 233, 1)),
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-      // body: SingleChildScrollView(
-      //     child: Column(
-      //   children: [
-      //     circleDemo(),
-      //     SizedBox(
-      //       height: 10,
-      //     ),
-      //     cardDemo(
-      //         emailText:
-      //             emailText), //when the cards are to be used, uncomment the section
-      //   ],
-      // )),
+        appBar: AppBar(
+          title: Text(
+            "HELLO",
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Color.fromRGBO(228, 205, 233, 1)),
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+        // body: SingleChildScrollView(
+        //     child: Column(
+        //   children: [
+        //     circleDemo(),
+        //     SizedBox(
+        //       height: 10,
+        //     ),
+        //     cardDemo(
+        //         emailText:
+        //             emailText), //when the cards are to be used, uncomment the section
+        //   ],
+        // )),
 
-      //body: Container(child: PageOne()),
+        //body: Container(child: PageOne()),
 
-      //body: Container(child: StackDemo()),
+        //body: Container(child: StackDemo()),
 
-      body: RoundedButton(
-          btnName: 'Login',
-          icon: Icon(Icons.lock),
-          callBack: () {
-            print("Logged In");
-          },
-          textStyle: TextStyle(
-          fontSize: 18,
-          color: const Color.fromARGB(255, 245, 160, 160),
-          fontWeight: FontWeight.w600 ,),
-    ));
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 50,
+                child: RoundedButton(
+                  btnName: 'play',
+                  icon: Icon(
+                    Icons.play_circle_fill,
+                    size: 28,
+                  ),
+                  callBack: () {
+                    print("Logged In");
+                  },
+                  textStyle: TextStyle(
+                    fontSize: 30,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Container(
+                height: 15,
+              ),
+              Container(
+                width: 150,
+                height: 50,
+                child: RoundedButton(
+                  btnName: 'seek',
+                  icon: Icon(
+                    Icons.forward_10_rounded,
+                    size: 25,
+                    color: const Color.fromARGB(255, 19, 140, 240),
+                  ),
+                  callBack: () {
+                    print("Played");
+                  },
+                  bgColor: Colors.amber,
+                  textStyle: TextStyle(
+                    fontSize: 28,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- git_flutter/lib/widgets_custom/RoundedBtn.dart
+++ git_flutter/lib/widgets_custom/RoundedBtn.dart
@@ -10,26 +10,41 @@ class RoundedButton extends StatelessWidget {
   const RoundedButton(
       {required this.btnName,
       this.icon,
-      this.bgColor = const Color.fromARGB(255, 223, 112, 242),
+      this.bgColor = const Color.fromARGB(255, 233, 147, 147),
       this.textStyle,
       this.callBack});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        onPressed: () {
-          callBack!();
-        },
-        child: icon!=null ? Row(
-          children: [
-            icon!,
-            Text(btnName,style: textStyle,)
-          ],
-        ): Text(btnName,style: textStyle,),
-        style: ElevatedButton.styleFrom(
-          backgroundColor:bgColor,
-          
-        ),
-        );
+      onPressed: () {
+        callBack!();
+      },
+      child: icon != null
+          ? Row(
+              children: [
+                icon!,
+                Container(
+                  height: 50,
+                  width: 15,
+                ),
+                Text(
+                  btnName,
+                  style: textStyle,
+                )
+              ],
+            )
+          : Text(
+              btnName,
+              style: textStyle,
+            ),
+      style: ElevatedButton.styleFrom(
+          backgroundColor: bgColor,
+          shadowColor: bgColor,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(21),
+                  bottomLeft: Radius.circular(21)))),
+    );
   }
 }
