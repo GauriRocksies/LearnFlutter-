f8ba988a5ab1f613c7818a75d103092c361c7ce4
GauriRocksies <gauri03uk@gmail.com>
2025-07-08 00:34:08 +0530

    Learnt the font awesome icons

--- git_flutter/lib/widgets_custom/icon.dart
+++ git_flutter/lib/widgets_custom/icon.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class IconDemo extends StatelessWidget {
   const IconDemo({super.key});
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Icon(
-      Icons.add_business_rounded,
-      size: 95,
-      color: Colors.black,
+        child: Column(
+      children: [
+        Icon(
+          Icons.add_business_rounded,
+          size: 195,
+          color: Colors.black,
+        ),
+        FaIcon(
+          FontAwesomeIcons.heartCircleCheck,
+          size: 243,
+          color: Color.fromARGB(255, 9, 239, 209),
+        )
+      ],
     ));
   }
 }
